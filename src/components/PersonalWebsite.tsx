30dc57d1bc23268a777f70c94faa964996ea3815
David Dawson <davedawson.co@gmail.com>
2025-02-03 22:04:04 +1100

    format() width

--- src/components/PersonalWebsite.tsx
+++ src/components/PersonalWebsite.tsx
@@ -45,7 +45,7 @@ const PersonalWebsite: React.FC = () => {
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-50 to-white">
-      <div className="max-w-4xl mx-auto p-4 sm:p-8">
+      <div className="mx-auto p-4 sm:p-8">
         <header className="text-center py-16 mb-12">
           <div className="w-32 h-32 mx-auto mb-8 rounded-full overflow-hidden ring-2 ring-gray-900/10 ring-offset-2 shadow-lg">
             <img 
